ef06767a662c50149514b34613547622440ab402
xhk <huangmumu121@163.com>
2020-06-02 22:55:41 +0800

    2255

--- 022-springboot-dubbo-ssm-interface/src/main/java/com/bjpowernode/springboot/bean/Student.java
+++ 022-springboot-dubbo-ssm-interface/src/main/java/com/bjpowernode/springboot/bean/Student.java
@@ -1,6 +1,8 @@
 package com.bjpowernode.springboot.bean;
 
-public class Student {
+import java.io.Serializable;
+
+public class Student implements Serializable {
     private Integer studentId;
 
     private String studentName;
--- 023-springboot-dubbo-ssm-provider/src/main/java/com/bjpowernode/springboot/Application.java
+++ 023-springboot-dubbo-ssm-provider/src/main/java/com/bjpowernode/springboot/Application.java
@@ -1,9 +1,13 @@
 package com.bjpowernode.springboot;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubboConfig;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.bjpowernode.springboot.mapper")
+@EnableDubboConfig //开启dubbo注解
 public class Application {
 
     public static void main(String[] args) {
